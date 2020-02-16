f3a9ef05996522d23a4a45ff36e0a47f4d7f9e31
Rafael <rafaelbarbosatec@gmail.com>
2020-02-15 21:12:27 -0300

    atualizacao do exemplo

---
+++ example/lib/screens/home/HomeView.dart
@@ -0,0 +1,88 @@
+import 'package:bsev/bsev.dart';
+import 'package:bsev_demo/repository/cripto_repository/model/Cripto.dart';
+import 'package:bsev_demo/screens/home/bloc/bloc.dart';
+import 'package:bsev_demo/screens/home/widgets/CriptoWidget.dart';
+import 'package:bsev_demo/screens/second_screen/SecondView.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  final GlobalKey<ScaffoldState> scaffoldStateKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Bsev<HomeBloc, HomeStreams>(
+      eventReceiver: (event, communication) {
+        if (event is HomeEventShowError) {
+          showSnackBar(event.msg, communication.dispatcher);
+        }
+      },
+      builder: (context, communication) {
+        return Scaffold(
+          key: scaffoldStateKey,
+          appBar: AppBar(),
+          body: Container(
+            child: Stack(
+              children: <Widget>[
+                _buildListStream(communication),
+                _buildProgressStream(communication.streams)
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildListStream(BlocCommunication<HomeStreams> communication) {
+    return RefreshIndicator(
+      onRefresh: () {
+        communication.dispatcher(HomeEventLoad());
+        return Future.value();
+      },
+      child: communication.streams.cryptoCoins.builder<List<Cripto>>((data) {
+        return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              if (index >= data.length - 1) {
+                communication.dispatcher(HomeEventLoad()..isMore = true);
+              }
+
+              return CryptoWidget(
+                item: data[index],
+                onClick: (item) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondView()),
+                  );
+                },
+              );
+            });
+      }),
+    );
+  }
+
+  Widget _buildProgressStream(HomeStreams streams) {
+    return streams.showProgress.builder<bool>((data) {
+      if (data) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      } else {
+        return Container();
+      }
+    });
+  }
+
+  void showSnackBar(String msg, dispatcher) {
+    scaffoldStateKey.currentState.showSnackBar(SnackBar(
+      content: Text(msg),
+      duration: Duration(seconds: 10),
+      action: SnackBarAction(
+        label: 'Try Again',
+        onPressed: () {
+          dispatcher(HomeEventLoad());
+        },
+      ),
+    ));
+  }
+}
---
+++ example/lib/screens/home/bloc/bloc.dart
@@ -0,0 +1,3 @@
+export 'package:bsev_demo/screens/home/bloc/HomeBloc.dart';
+export 'package:bsev_demo/screens/home/bloc/HomeEvents.dart';
+export 'package:bsev_demo/screens/home/bloc/HomeStreams.dart';
